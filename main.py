21736c694690f6ec4330f356046baec2d1eaa90c
PrinceJoshi312 <princejoshij736@gmail.com>
2025-05-16 18:01:47 +0530

    initial commit in chatbot

---
+++ main.py
@@ -0,0 +1,72 @@
+from flask import Flask, request, jsonify
+import json
+import random
+import pickle
+import numpy as np
+from keras import models
+import nltk
+from nltk.stem import WordNetLemmatizer
+
+lemmatizer = WordNetLemmatizer()
+intents = json.loads(open('intents.json').read())
+words = pickle.load(open('words.pkl', 'rb'))
+classes = pickle.load(open('classes.pkl', 'rb'))
+model = models.load_model('chatbot.h5')
+
+app = Flask(__name__)
+
+# Manually add CORS headers
+@app.after_request
+def add_cors_headers(response):
+    response.headers['Access-Control-Allow-Origin'] = '*'  # allow all origins
+    response.headers['Access-Control-Allow-Methods'] = 'GET, POST, OPTIONS'
+    response.headers['Access-Control-Allow-Headers'] = 'Content-Type, Authorization'
+    return response
+
+def clean_up_sentence(sentence):
+    sentence_words = nltk.word_tokenize(sentence)
+    sentence_words = [lemmatizer.lemmatize(word) for word in sentence_words]
+    return sentence_words
+
+def bag_of_words(sentence):
+    sentence_words = clean_up_sentence(sentence)
+    bag = [0] * len(words)
+    for w in sentence_words:
+        for i, word in enumerate(words):
+            if word == w:
+                bag[i] = 1
+    return np.array(bag)
+
+def predict_class(sentence):
+    bow = bag_of_words(sentence)
+    res = model.predict(np.array([bow]))[0]
+    error_threshold = 0.25
+    results = [[i, r] for i, r in enumerate(res) if r > error_threshold]
+    results.sort(key=lambda x: x[1], reverse=True)
+    return_list = [{"intent": classes[r[0]], "probability": str(r[1])} for r in results]
+    return return_list
+
+def get_response(intents_list, intents_json):
+    if not intents_list:
+        return "Sorry, I didn't understand that. Can you rephrase?"
+    tag = intents_list[0]['intent']
+    for intent in intents_json['intents']:
+        if intent['tags'] == tag:
+            return random.choice(intent['responces'])
+
+@app.route("/chat", methods=["GET", "POST"])
+def chat():
+    if request.method == "GET":
+        return "Chatbot endpoint. Please send a POST request with JSON."
+
+    data = request.get_json()
+    message = data.get("message")
+    if not message:
+        return jsonify({"response": "Please send a message."})
+    intents_list = predict_class(message)
+    response = get_response(intents_list, intents)
+    return jsonify({"response": response})
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
